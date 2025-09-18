a67fa48de1e129b263f5f80e78e613dd4c0f015d
Manzick <manzurov.alex@yahoo.com>
2025-09-18 18:30:26 +0600

    Color fix

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "CarrotSDK",
-            url: "https://github.com/carrotquest/carrotquest-ios-spm/releases/download/2.13.3/CarrotSDK.xcframework.zip",
-            checksum: "1a07dd1061b430ffe2cdf95ca2aeb1ab62e18297e14c61ad7dd03656ffbc5b08"
+            url: "https://github.com/carrotquest/carrotquest-ios-spm/releases/download/2.13.5/CarrotSDK.xcframework.zip",
+            checksum: "43f7b90ebdeb8991425d2f83dd8b298f6aa2029512d7cdc6f8b2b7c2e0249f57"
         ),
     ]
 )
