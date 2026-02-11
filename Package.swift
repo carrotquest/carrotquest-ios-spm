65545d76d46508a6b7d2444b0247df873e593e97
Manzick <manzurov.alex@yahoo.com>
2026-02-11 15:19:45 +0600

    Pop-Up & UI improvements

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "CarrotSDK",
-            url: "https://github.com/carrotquest/carrotquest-ios-spm/releases/download/3.1.1/CarrotSDK.xcframework.zip",
-            checksum: "075b4d2d175e6cac72f911fc3f3acdd48ca53af4825a2557aef25e9c855e4e96"
+            url: "https://github.com/carrotquest/carrotquest-ios-spm/releases/download/3.1.2/CarrotSDK.xcframework.zip",
+            checksum: "7699a32616cde604c8459a60a0dcca463ce82b464dda4a3657acc0d248028431"
         ),
     ]
 )
