200e0384c3a1af73f2ba1bbffe459c4fbf080252
Manzick <manzurov.alex@yahoo.com>
2026-04-08 19:48:13 +0600

    fix some bugs 3.1.3

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "CarrotSDK",
-            url: "https://github.com/carrotquest/carrotquest-ios-spm/releases/download/3.1.2/CarrotSDK.xcframework.zip",
-            checksum: "7699a32616cde604c8459a60a0dcca463ce82b464dda4a3657acc0d248028431"
+            url: "{URL}",
+            checksum: "{CHECKSUM}"
         ),
     ]
 )
