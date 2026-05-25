4459e16c61583004ff495cd03465182d62ebdfaa
Manzick <manzurov.alex@yahoo.com>
2026-05-25 23:08:01 +0600

    Improved UTM handling 3.1.5

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "CarrotSDK",
-            url: "{URL}",
-            checksum: "{CHECKSUM}"
+            url: "https://github.com/carrotquest/carrotquest-ios-spm/releases/download/3.1.5/CarrotSDK.xcframework.zip",
+            checksum: "7805c0ef9d393f8e4d58fba3f1f8bc75c5fa4572f905dfbac1ea4d0abde46b06"
         ),
     ]
 )
